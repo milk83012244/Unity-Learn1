2b82b301f7f91355cf05a60b86557b5c51986141
milk83012244 <milk83012244@gmail.com>
2019-06-05 13:11:09 +0800

    新增狗年齡轉換 吃東西方法

--- Unity Learn1/Assets/Scripts/Dog.cs
+++ Unity Learn1/Assets/Scripts/Dog.cs
@@ -13,10 +13,38 @@ public class Dog
     public float weight;
     #endregion
     #region 方法
+    /// <summary>
+    /// 狗狗叫的方法
+    /// </summary>
     public void Bark()
     {
         Debug.Log(name + ":汪汪");
     }
-    public
+    /// <summary>
+    /// 轉換狗狗年齡方法
+    /// </summary>
+    /// <returns>狗狗年齡*7</returns>
+    public int ConvertAge()
+    {
+        return age * 7;
+    }
+    /// <summary>
+    /// 吃方法
+    /// </summary>
+    /// <param name="food">食物</param>
+    /// <param name="speed">吃飯速度</param>
+    public void Eat(string food, string speed = "慢")
+    {
+        Debug.Log(name + " 正在吃" + food + " 吃飯速度:" + speed);
+    }
+    /// <summary>
+    /// 吃方法多載
+    /// </summary>
+    /// <param name="food">食物</param>
+    /// <param name="count">份量</param>
+    public void Eat(string food , int count)
+    {
+        Debug.Log(name + " 正在吃" + food + " 份量" + count);
+    }
     #endregion
 }
