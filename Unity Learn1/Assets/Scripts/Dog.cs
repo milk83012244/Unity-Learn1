75c84497f50df4f424dcf4abaa88b6ea96f123fe
milk83012244 <milk83012244@gmail.com>
2019-06-05 11:41:13 +0800

    新增狗類別欄位與叫方法

---
+++ Unity Learn1/Assets/Scripts/Dog.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Dog
+{
+    #region 欄位
+    public string name;
+    public string type;
+    public string sex;
+    public string color;
+
+    public int age;
+
+    public float weight;
+    #endregion
+    #region 方法
+    public void Bark()
+    {
+        Debug.Log(name + ":汪汪");
+    }
+    public
+    #endregion
+}
